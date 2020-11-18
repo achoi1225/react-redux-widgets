1927dff91981fb72723a9c4d84f04ff8c5ef6a59
Scott Smith <scott.sncsmith@gmail.com>
2020-11-18 15:53:35 -0800

    Finish folders

---
+++ src/Folder.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route, NavLink} from 'react-router-dom';
+
+const Folder = ({folders}) => {
+  return (
+    <BrowserRouter>
+      <div class="folders">
+        <h1>Tabs</h1>
+        <div className='tabs'>
+          <ul className='tab-header'>
+            {folders.map((folder, idx) => {
+              return (
+              <li key={idx}>
+                <NavLink to={`/tabs/${folder.title}`}>{folder.title}</NavLink>
+              </li>
+              )
+            })}
+          </ul>
+          <Switch>
+            {folders.map((folder, idx) => {
+              return (
+              <Route key={folder.title} exact path={
+                idx === 0 ?
+                [`/tabs/${folder.title}`, '/'] :
+                `/tabs/${folder.title}`}>
+                <div className="tab-content">
+                  {folder.content}
+                </div>
+              </Route>
+              )
+            })}
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default Folder
--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Clock from './Clock';
+import Folder from './Folder';
 
 function Root() {
+  const folders = [
+    {title: 'one', content: 'I am the first'},
+    {title: 'two', content: 'Second folder here'},
+    {title: 'three', content: 'Third folder here'}
+  ];
   return (
     <div>
       <Clock />
+      <Folder folders={folders}/>
     </div>
   );
 }
